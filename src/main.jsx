8ec4a1c0a4fa52d41e6bbc17b119930eee326599
john-cpu25 <nhan.nguyenthanh0894@gmail.com>
2026-05-28 17:27:14 +0700

    Restore MSAL implementation to allow local PC email detection

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { MsalProvider } from '@azure/msal-react'
+import { msalInstance } from './services/msalInstance'
 import { AppProvider } from './context/AppContext.jsx'
 import { AuthProvider } from './context/AuthContext'
 import { NotificationProvider } from './context/NotificationContext'
@@ -11,13 +13,16 @@ import { ErrorBoundary } from './ErrorBoundary.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ErrorBoundary>
-      <AuthProvider>
-        <NotificationProvider>
-          <AppProvider>
-            <App />
-          </AppProvider>
-        </NotificationProvider>
-      </AuthProvider>
+      <MsalProvider instance={msalInstance}>
+        <AuthProvider>
+          <NotificationProvider>
+            <AppProvider>
+              <App />
+            </AppProvider>
+          </NotificationProvider>
+        </AuthProvider>
+      </MsalProvider>
     </ErrorBoundary>
   </StrictMode>,
 )
+
